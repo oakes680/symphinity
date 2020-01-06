6dbb6fa619d6c32f206c649b2c319c4d215d0dc7
leza jackson <thenativedev@outlook.com>
2020-01-06 15:41:19 -0800

    axios call and private router done

---
+++ song_suggester/src/utils/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+    const token = `Bearer ${localStorage.getItem('token')}`
+    
+    return axios.create({
+        baseUrl: ' https://spotify-song-suggester-be.herokuapp.com/api/auth/',
+        headers: {
+            "Authorization" : token
+        }
+    })
+
+}
