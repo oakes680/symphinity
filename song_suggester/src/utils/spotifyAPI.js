190ebd08c3dfc52281a556580184d592bb9e0a82
agohorel <gohorelalex@gmail.com>
2020-01-10 00:52:49 -0500

    update spotify API redirect
    https://songsuggester-ewmsbuz2h.now.sh/dashboard

--- song_suggester/src/utils/spotifyAPI.js
+++ song_suggester/src/utils/spotifyAPI.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://accounts.spotify.com/authorize";
 const clientID = "46e78711e2c14c21a689149e27b79266";
-const redirect = "http://localhost:3000/dashboard";
+const redirect = "https://songsuggester-ewmsbuz2h.now.sh/dashboard";
 const scope = "user-read-private";
 export const url = `${baseUrl}?client_id=${clientID}&redirect_uri=${redirect}&scope=${scope}&response_type=token`;
